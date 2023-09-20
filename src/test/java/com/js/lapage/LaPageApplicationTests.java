b2fd0c1f6b06af944c73f635df218465164c3a6d
JuliaDosSantos <santos.ju1995@gmail.com>
2023-09-19 22:00:48 -0300

    first commit

---
+++ src/test/java/com/js/lapage/LaPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js.lapage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
