5aba5d7e3c624c56fce996e3d4f37e6111636e36
JuliaDosSantos <santos.ju1995@gmail.com>
2023-09-26 23:32:19 -0300

    Classe Usuario

---
+++ src/main/java/com/js/lapage/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.js.lapage.repository;
+
+import com.js.lapage.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+}
